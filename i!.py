05c6adf032f41db1eb076de17bc8b3c1564f1e1a
LeMinaw <leminaw@gmail.com>
2016-10-10 21:51:13 +0200

    Added functionnal i! encoding support.

--- i!.py
+++ i!.py
@@ -1,32 +1,45 @@
-#!usr/env python3
+#!/usr/bin/env python3
 
-ALPHABET = {0: '!',
-            1: 'i'}
+# A module implementing a custom and fun binary language composed of i/¡ and !.
+
+ALPHABET = {'0': '!',
+            '1': '¡'} # ¡ or i
 
 ALPHABET_DECODE = dict(zip(ALPHABET.values(), ALPHABET.keys())) # Reverses ALPHABET keys and values
 
+
 def encode(data_in):
     """Encodes an int to i! str."""
-    
+
     digits = list(data_in)
     bytes = []
     for digit in digits:
-        bytes.append(bin(digit))
-    
+        bytes.append(bin(int(digit)))
+
     data_out = ''
     for byte in bytes:
-        for bit in byte:
+        data_out += ALPHABET['0'] * (8 - len(byte[2:])) # Adding '0s' to reach for 8 bits byte
+        for bit in byte[2:]: # Skipping '0b'
             data_out += ALPHABET[bit]
-    
+
     return data_out
+
 
 def decode(data_in):
     """Decodes a i! str to an int."""
-    
-    chars = list(data_int)
+
+    chars = list(data_in)
     data_out = ''
-    
+    group = ''
     for char in chars:
-        data_out += ALPHABET_DECODE[char]
-    
-    return int(bin(int(data_out)))
+        group += ALPHABET_DECODE[char]
+        if len(group) == 8:
+            data_out += str(int(group, 2))
+            group = ''
+
+    return int(data_out)
+
+
+if __name__ == '__main__':
+    print(encode(input("Int to encode: ")))
+    print(decode(input("Str to decode: ")))
